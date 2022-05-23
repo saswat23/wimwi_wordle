60396aa7bc8bdc72d2c16c41ad4b2eff8525fb50
saswat23 <saswata.panigrahi@gmail.com>
2022-05-24 00:42:43 +0530

    Update iima.js

--- iima.js
+++ iima.js
@@ -131,7 +131,7 @@ function update() {
     console.log(guess);
 
     if (!guessList.includes(guess)) {
-        document.getElementById("answer").innerText = "Not in word list";
+        document.getElementById("answer").innerText = "Not a WIMWI word, enter another word";
         return;
     }
     
